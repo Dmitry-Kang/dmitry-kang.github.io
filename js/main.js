b71fd94ad81bac7596016f9618bab06c62e4c2e2
Dmitriy Alexandrov <dmitriy.alexandrov.adci@gmail.com>
2022-02-13 00:45:09 +0600

    s

--- js/main.js
+++ js/main.js
@@ -29,3 +29,42 @@ Array.prototype.forEach.call(elems2, function (element) {
         }
     })
 });
+
+let button1 = document.querySelectorAll('.button1');
+let button2 = document.querySelectorAll('.button2');
+let button3 = document.querySelectorAll('.button3');
+
+Array.prototype.forEach.call(button1, function (element) {
+    element.addEventListener('click', function(){    
+        allDisabled();
+        clearDisabled();
+    })
+});
+Array.prototype.forEach.call(button2, function (element) {
+    element.addEventListener('click', function(){    
+        allDisabled();
+        clearDisabled();
+    })
+});
+Array.prototype.forEach.call(button3, function (element) {
+    element.addEventListener('click', function(){    
+        allDisabled();
+        clearDisabled();
+    })
+});
+
+const allDisabled = () => {
+    
+    document.querySelector('.button1').classList.toggle('disabled', true);
+    document.querySelector('.button2').classList.toggle('disabled', true);
+    document.querySelector('.button3').classList.toggle('disabled', true);
+  
+}
+
+const clearDisabled = () => {
+    setTimeout(()=>{
+        document.querySelector('.button1').classList.toggle('disabled', false);
+        document.querySelector('.button2').classList.toggle('disabled', false);
+        document.querySelector('.button3').classList.toggle('disabled', false);
+    }, 1100)
+}
