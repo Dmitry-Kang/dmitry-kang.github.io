ba078290162b9b0a6e3828b65546fb2133beeed8
Dmitry-Kang <alexandrov.dp@mail.ru>
2022-11-10 01:15:54 +0600

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const gulp = require('gulp');
+var watch = require('gulp-watch');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp.src('./style/sass/**/*.scss')
+  return gulp.src('./style/sass/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./style'));
 };
 
 exports.buildStyles = buildStyles;
 exports.watch = function () {
-  gulp.watch('./style/sass/**/*.scss', ['sass']);
+  gulp.watch('./style/sass/*.scss', buildStyles);
 };
